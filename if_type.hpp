1769face87d6b8ca17d09f08fa62d8c99babd4e2
Ricky Doofenshmirtz <RickyDoofenshmirtz@gmail.com>
2025-03-26 17:22:57 +0530

    added push back and pop back for type_list

--- if_type.hpp
+++ if_type.hpp
@@ -1,16 +1,20 @@
-template <bool condition, typename THEN, typename ELSE>
-struct if_type;
-
-template <typename T, typename E>
-struct if_type<true, T, E>
+template <typename T>
+struct has_type
 {
     using type = T;
 };
 
+template <bool condition, typename THEN, typename ELSE>
+struct if_type;
+
 template <typename T, typename E>
-struct if_type<false, T, E>
+struct if_type<true, T, E> : has_type<T>
 {
-    using type = E;
+};
+
+template <typename T, typename E>
+struct if_type<false, T, E> : has_type<E>
+{
 };
 
 template <bool condition, typename T, typename E>
--- type_list.hpp
+++ type_list.hpp
@@ -1,5 +1,7 @@
 #include "if_type.hpp"
 
+#include <corecrt.h>
+#include <cstddef>
 #include <list>
 #include <string>
 #include <type_traits>
@@ -12,6 +14,9 @@ inline auto contain(const std::string& search, std::list<std::string>& l)
     l.pop_front();
     return contain(search, l);
 }
+
+//////////////////////////////////////////////////////////////////////////////////////
+// type list
 
 template <typename...>
 struct type_list
@@ -30,16 +35,21 @@ struct empty<type_list<>> : std::true_type
 
 static_assert(empty<type_list<>>::value);
 
+//////////////////////////////////////////////////////////////////////////////////////
+// front
+
 template <typename LIST>
 struct front;
 
 template <typename T0, typename... T1toN>
-struct front<type_list<T0, T1toN...>>
+struct front<type_list<T0, T1toN...>> : has_type<T0>
 {
-    using type = T0;
 };
 
 static_assert(std::is_same_v<front<type_list<int, bool, double>>::type, int>);
+
+//////////////////////////////////////////////////////////////////////////////////////
+// pop front
 
 template <typename LIST>
 struct pop_front
@@ -47,9 +57,8 @@ struct pop_front
 };
 
 template <typename T0, typename... T1toN>
-struct pop_front<type_list<T0, T1toN...>>
+struct pop_front<type_list<T0, T1toN...>> : has_type<type_list<T1toN...>>
 {
-    using type = type_list<T1toN...>;
 };
 
 template <typename LIST>
@@ -60,6 +69,101 @@ using pop_front_t = typename pop_front<LIST>::type;
 
 template <typename LIST>
 static constexpr bool empty_v = empty<LIST>::value;
+
+//////////////////////////////////////////////////////////////////////////////////////
+// back
+
+/*
+    variadic templates can only be used as the last parameter, so we will have
+    to use recursion
+*/
+
+template <typename LIST>
+struct back : has_type<typename back<pop_front_t<LIST>>::type>
+{
+};
+
+template <typename T0>
+struct back<type_list<T0>> : has_type<T0>
+{
+};
+
+template <typename LIST>
+using back_t = typename back<LIST>::type;
+
+static_assert(std::is_same_v<back_t<type_list<int, bool, double>>, double>);
+static_assert(std::is_same_v<back_t<type_list<int, double, bool>>, bool>);
+
+//////////////////////////////////////////////////////////////////////////////////////
+// push back
+
+template <typename LIST, typename T>
+struct push_back
+{
+};
+
+template <typename... T0toN, typename T>
+struct push_back<type_list<T0toN...>, T> : has_type<type_list<T0toN..., T>>
+{
+};
+
+template <typename LIST, typename T>
+using push_back_t = typename push_back<LIST, T>::type;
+
+static_assert(std::is_same_v<push_back_t<type_list<>, int>, type_list<int>>);
+static_assert(std::is_same_v<
+              push_back_t<type_list<int, bool>, double>,
+              type_list<int, bool, double>>);
+
+//////////////////////////////////////////////////////////////////////////////////////
+// pop back
+
+template <typename LIST, typename RET_LIST = type_list<>>
+struct pop_back;
+
+template <typename T0, typename RET_LIST>
+struct pop_back<type_list<T0>, RET_LIST> : has_type<RET_LIST>
+{
+};
+
+template <typename T0, typename T1, typename... T2toN, typename RET_LIST>
+struct pop_back<type_list<T0, T1, T2toN...>, RET_LIST>
+    : pop_back<type_list<T1, T2toN...>, push_back_t<RET_LIST, T0>>
+{
+};
+
+template <typename LIST>
+using pop_back_t = typename pop_back<LIST>::type;
+
+static_assert(std::is_same_v<pop_back_t<type_list<int>>, type_list<>>);
+static_assert(std::is_same_v<
+              pop_back_t<type_list<int, bool, double>>,
+              type_list<int, bool>>);
+
+//////////////////////////////////////////////////////////////////////////////////////
+// at
+
+template <typename LIST, std::size_t index>
+struct at;
+
+template <typename LIST, std::size_t index>
+struct at : has_type<typename at<pop_front_t<LIST>, index - 1>::type>
+{
+};
+
+template <typename LIST>
+struct at<LIST, 0> : has_type<front_t<LIST>>
+{
+};
+
+template <typename LIST, std::size_t index>
+using at_t = typename at<LIST, index>::type;
+
+static_assert(
+    std::is_same_v<typename at<type_list<int, bool, double>, 1>::type, bool>);
+
+//////////////////////////////////////////////////////////////////////////////////////
+// contains type
 
 template <typename SEARCH, typename LIST>
 struct contains_type;
@@ -76,6 +180,8 @@ template <typename SEARCH>
 struct contains_type<SEARCH, type_list<>> : std::false_type
 {
 };
+
+//////////////////////////////////////////////////////////////////////////////////////
 
 type_list<int, bool, double> types;
 
